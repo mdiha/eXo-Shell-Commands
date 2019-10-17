35471526f19fe1e72a77fed7b22e051db8ea7df3
Houssem <hbenali@exoplatform.com>
2019-10-17 12:59:25 +0200

    Fix exoupdate

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.exocmd/ &> /dev/null
 wkdir="$1"
 if [ -z "$wkdir" ]; then wkdir="$(pwd)" ; fi
 cp -rf "$wkdir"/* ~/.exocmd/ &> /dev/null
+cp -rf "$wkdir"/.git ~/.exocmd/ &> /dev/null
 if [ -z "$( cat ~/.bashrc | grep exocmd/custom.sh)" ]; then
 echo "source ~/.exocmd/custom.sh" >> ~/.bashrc
 fi
